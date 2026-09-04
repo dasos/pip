801f0eaf54a3542e2ebea4a5433278100643c025
Martin <martin@dasos.com>
2026-09-04 09:59:44 +0000

    Fix phone system bar visibility

--- phone/src/main/java/com/pip/phone/ui/MainActivity.kt
+++ phone/src/main/java/com/pip/phone/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.pip.phone.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -15,6 +16,7 @@ import com.pip.phone.ui.theme.PipPhoneTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         val config = ServerConfig(this)
 
         setContent {
