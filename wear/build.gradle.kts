14dae319fd8118cea5812e2d4beae0aa321b382c
Martin <martin@dasos.com>
2026-09-14 22:22:11 +0000

    Release v0.2.4

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = "com.pip"
         minSdk = 30
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.2.3"
+        versionCode = 6
+        versionName = "0.2.4"
     }
 
     buildTypes {
